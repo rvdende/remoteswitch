e564fa6a48bdbdb78d76d67813ed64df105e4052
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2022-12-04 16:09:03 +0200

    rx tx packetcount

--- src/server/mqtt/mqttserver.ts
+++ src/server/mqtt/mqttserver.ts
@@ -10,35 +10,40 @@ import { v4 as uuidv4 } from 'uuid';
 import * as mqttpacket from 'mqtt-packet'
 import { isJson } from "@/utils/isjson";
 import { ee } from "../trpc/router/realtime";
+import { prisma } from '@/server/db/client'
+import { z } from "zod";
+import { deviceDataSchema } from "@/validation/devicedata";
+import { handleMqttPacket } from "./packet";
+import type { Socket } from "net";
 
-interface ICorePacket {
-    id: string
-}
+// interface ICorePacket {
+//     id: string
+// }
 
-interface IUser {
-    id: string
-    name: string
-    apikey?: string
-}
+// interface IUser {
+//     id: string
+//     name: string
+//     apikey?: string
+// }
 
-export interface Subscription {
-    /** apikey to authenticate subscription */
-    apikey: string
-    /** id of the device to subscribe to */
+
+
+// interface MQTTClient {
+//     id: string
+//     socket: net.Socket
+//     subscriptions: Subscription[]
+//     apikey: string
+// }
+
+
+export interface SocketExtended extends Socket {
     id?: string
-}
-
-interface MQTTClient {
-    id: string
-    socket: net.Socket
-    subscriptions: Subscription[]
-    apikey: string
 }
 
 export class MQTTServer extends EventEmitter {
     server: net.Server;
 
-    connectedClients: MQTTClient[] = [];
+    // connectedClients: MQTTClient[] = [];
 
     constructor() {
         super();
@@ -46,11 +51,11 @@ export class MQTTServer extends EventEmitter {
         this.server.listen(1883);
     }
 
-    handleClient = (socket: net.Socket) => {
+    handleClient = (socket: SocketExtended) => {
         const opts = { protocolVersion: 4 }; // default is 4. Usually, opts is a connect packet
         const parser = mqttpacket.parser(opts);
 
-        const id = uuidv4();
+        socket.id = uuidv4();
         console.log('mqtt new socket client');
 
         socket.on('data', (data) => {
@@ -62,205 +67,88 @@ export class MQTTServer extends EventEmitter {
             socket.end();
             socket.destroy()
             //clearInterval(pinger);
-            this.removeClient(id);
+            // this.removeClient(id);
         })
 
         socket.on("connect", () => { console.log(`mqtt client connect`); })
         socket.on("drain", () => { console.log(`mqtt client drain`); })
         socket.on("end", () => { console.log(`mqtt client end`); });
-        socket.on("error", (err) => { console.log(`mqtt client error: ${err.message}`); })
+        // socket.on("error", (err) => { console.log(`mqtt client error: ${err.message}`); })
         socket.on("timeout", () => { console.log(`mqtt client timeout`); })
 
-        socket.on("lookup", (err, address, family, host) => {
-            console.log(`mqtt client lookup ${{ err, address, family, host }}`);
-        })
+        // socket.on("lookup", (err, address, family, host) => {
+        //     console.log(`mqtt client lookup ${{ err, address, family, host }}`);
+        // })
 
-        parser.on('packet', async packet => {
-            if (packet.cmd == "connect") {
-                console.log(`packet`, packet)
-
-                // deny
-                // socket.write(mqttpacket.generate({
-                //     cmd: "connack",
-                //     returnCode: 4,
-                //     sessionPresent: false
-                // }))
-
-
-
-                const client: MQTTClient = {
-                    id,
-                    socket,
-                    subscriptions: [],
-                    apikey: "test"
-                }
-
-                // keep track of client
-                this.addClient(client);
-
-                socket.write(mqttpacket.generate({
-                    cmd: 'connack',
-                    returnCode: 0,
-                    sessionPresent: true
-                }));
-            }
-
-            //// - - - - -
-            if (packet.cmd == "subscribe") {
-                let count = 0;
-                const new_subscriptions: Subscription[] = [];
-
-                for (const sub of packet.subscriptions) {
-                    const subraw = sub.topic;
-                    if (isJson(subraw)) {
-                        const new_sub: Subscription = JSON.parse(subraw);
-                        if (new_sub.apikey) {
-                            new_subscriptions.push(new_sub)
-                        }
-                    } else {
-                        const new_sub: Subscription = { apikey: sub.topic, id };
-                        new_subscriptions.push(new_sub)
-                    }
-                    count++;
-                    if (count == packet.subscriptions.length) {
-
-                        this.addSubscriptionsToUser(id, new_subscriptions);
-
-                        socket.write(mqttpacket.generate({
-                            cmd: 'suback',
-                            messageId: packet.messageId,
-                            granted: [0]
-                        }));
-                    }
-                }
-            }
-            //// - - - - -
-            if (packet.cmd == "pingreq") {
-                // console.log('mqtt ping resp')
-                // updates last seen and so on so we know the device is alive.
-
-                // TODO update db entry lastSeen datetime if authed
-
-                socket.write(mqttpacket.generate({
-                    cmd: 'pingresp'
-                }))
-            }
-
-            if (packet.cmd == "publish") {
-                console.log(packet)
-
-                if (!isJson(packet.payload.toString())) return;
-
-                const payloadJSON = JSON.parse(packet.payload.toString());
-
-                console.log(payloadJSON);
-
-                ee.emit('add', payloadJSON);
-                // if (isJson(packet.payload)) {
-                //     const corepacket: ICorePacket = JSON.parse(packet.payload.toString());
-                //     corepacket.userid = authed_user.userid
-
-                //     if (!thisconnectionApikey) return;
-
-                //     let result = await handleCorePacket(
-                //         thisconnectionApikey, // authed_user.apikey[0],
-                //         corepacket,
-                //         this.props.db,
-                //         this.props,
-                //         'mqtt'
-                //     ).catch(err => {
-                //         console.log(err);
-                //     });
-                //     if (result) {
-                //         // console.log('PUBLISH SUCCESS', corepacket);
-                //         last_packet = corepacket;
-                //         if (packet.qos > 0) {
-                //             socket.write(mqttpacket.generate({
-                //                 cmd: 'puback',
-                //                 messageId: packet.messageId
-                //             }))
-                //         }
-                //     }
-                // } else {
-                //     console.log('not valid corepacket? ', packet.payload.toString())
-                // }
-
-                if (packet.qos == 0) return;
-
-                socket.write(mqttpacket.generate({
-                    cmd: 'puback',
-                    messageId: packet.messageId
-                }))
-
-            }
-
-        })
+        parser.on('packet', (packet) => {
+            handleMqttPacket(socket, packet);
+        });
 
     }
 
     /**
      * sends out packet to subscribed clients
      */
-    broadcast(apikey: string, packet: ICorePacket) {
-        // console.log('MQTT clients connected:',this.connectedClients.length)
-        for (const client of this.connectedClients) {
-            // console.log('client.subscriptions:',client.subscriptions)                 
-            for (const sub of client.subscriptions) {
-                if (sub.apikey === apikey) {
-                    console.log(`${new Date().toString()} sending mqtt`)
-                    const send = true;
-                    // if (sub.id) {
-                    //     if (sub.id !== packet.id) { send = false; }
-                    // }
-                    if (send) {
-                        const cleanpacket: any = JSON.parse(JSON.stringify(packet));
-                        // delete cleanpacket._id;
-                        delete cleanpacket.userid;
-                        delete cleanpacket.timestamp;
+    // broadcast(apikey: string, packet: ICorePacket) {
+    //     // console.log('MQTT clients connected:',this.connectedClients.length)
+    //     for (const client of this.connectedClients) {
+    //         // console.log('client.subscriptions:',client.subscriptions)                 
+    //         for (const sub of client.subscriptions) {
+    //             if (sub.apikey === apikey) {
+    //                 console.log(`${new Date().toString()} sending mqtt`)
+    //                 const send = true;
+    //                 // if (sub.id) {
+    //                 //     if (sub.id !== packet.id) { send = false; }
+    //                 // }
+    //                 if (send) {
+    //                     const cleanpacket: any = JSON.parse(JSON.stringify(packet));
+    //                     // delete cleanpacket._id;
+    //                     delete cleanpacket.userid;
+    //                     delete cleanpacket.timestamp;
 
-                        console.log(`mqtt connected count: ${this.connectedClients.length} broadcasting to ${client.apikey} ${client.id} `)
-                        client.socket.write(mqttpacket.generate({
-                            cmd: 'publish',
-                            topic: sub.apikey,
-                            payload: Buffer.from(JSON.stringify(cleanpacket)),
-                            qos: 0,
-                            dup: false,
-                            retain: false
-                        }));
-                    }
-                }
-            }
-        }
-    }
+    //                     console.log(`mqtt connected count: ${this.connectedClients.length} broadcasting to ${client.apikey} ${client.id} `)
+    //                     client.socket.write(mqttpacket.generate({
+    //                         cmd: 'publish',
+    //                         topic: sub.apikey,
+    //                         payload: Buffer.from(JSON.stringify(cleanpacket)),
+    //                         qos: 0,
+    //                         dup: false,
+    //                         retain: false
+    //                     }));
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
-    addClient(client: MQTTClient) {
-        let found = false;
-        for (const c of this.connectedClients) {
-            if (c.id === client.id) {
-                found = true;
-            }
-        }
-        if (found === false) {
-            this.connectedClients.push(client);
-            console.log(`mqtt adding client ${client.id} connected count after add: ${this.connectedClients.length}`);
-        }
-    }
+    // addClient(client: MQTTClient) {
+    //     let found = false;
+    //     for (const c of this.connectedClients) {
+    //         if (c.id === client.id) {
+    //             found = true;
+    //         }
+    //     }
+    //     if (found === false) {
+    //         this.connectedClients.push(client);
+    //         console.log(`mqtt adding client ${client.id} connected count after add: ${this.connectedClients.length}`);
+    //     }
+    // }
 
-    addSubscriptionsToUser(id: string, subscriptions: Subscription[]) {
-        for (const client of this.connectedClients) {
-            if (client.id == id) {
-                for (const sub of subscriptions) {
-                    if (client.subscriptions.indexOf(sub) == -1) {
-                        client.subscriptions.push(sub);
-                    }
-                }
-            }
-        }
-    }
+    // addSubscriptionsToUser(id: string, subscriptions: Subscription[]) {
+    //     for (const client of this.connectedClients) {
+    //         if (client.id == id) {
+    //             for (const sub of subscriptions) {
+    //                 if (client.subscriptions.indexOf(sub) == -1) {
+    //                     client.subscriptions.push(sub);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
-    removeClient = (id: string) => {
-        const filtered = this.connectedClients.filter(i => (i.id != id))
-        this.connectedClients = filtered;
-        console.log(`mqtt removed client ${id} connected count: ${this.connectedClients.length}`);
-    }
+    // removeClient = (id: string) => {
+    //     const filtered = this.connectedClients.filter(i => (i.id != id))
+    //     this.connectedClients = filtered;
+    //     console.log(`mqtt removed client ${id} connected count: ${this.connectedClients.length}`);
+    // }
 }
--- tests/denomqttclient/mqttclient.ts
+++ tests/denomqttclient/mqttclient.ts
@@ -9,27 +9,65 @@ await client.connect();
 console.log('connected?')
 
 
-// console.log("subscribing");
-// await client.subscribe('mqtt');
-// console.log("subscribed");
+console.log("subscribing");
+await client.subscribe('mqtt');
+console.log("subscribed");
 
 client.on('message', (topic: string, payload: string) => {
   console.log(topic, payload);
 });
 
-console.log('publish')
-
-console.log('done');
-
 let id = 0;
+
+interface PacketSchema {
+  id?: number | undefined;
+  uuid?: string | undefined;
+  type?: string | undefined;
+  description?: string | undefined;
+  outputs: DataUnion[]
+  inputs: DataUnion[]
+}
+
+type DataUnion = ({
+  type: "number";
+  description: string;
+  name: string;
+  value: number;
+} | {
+  type: "string";
+  description: string;
+  name: string;
+  value: string;
+} | {
+  type: "boolean";
+  description: string;
+  name: string;
+  value: boolean;
+})
+
+
 
 setInterval(async () => {
   id++;
-  await client.publish('mqtt', JSON.stringify({
-    id,
-    uuid: "616781a7-4ab5-45e3-96cc-a97233b0df02",
-    data: { temperature: Math.random()*100 }
-  }));
+
+  const data: PacketSchema = {
+    id, // optional packet number. useful if you want replies to queries.
+    uuid: "616781a7-4ab5-45e3-96cc-a97233b0df02", // must be on the first packet.. may be on every packet
+    inputs: [{
+      name: "Relay A",
+      description: "Switch AC Power. True is on.",
+      type: "boolean",
+      value: false
+    }],
+    outputs: [{
+      name: "temperature",
+      description: "ambient air temperature",
+      type: "number",
+      value: Math.random() * 100
+    }] 
+  }
+
+  await client.publish('mqtt', JSON.stringify(data));
 }, 1000)
 
 // await client.disconnect();
